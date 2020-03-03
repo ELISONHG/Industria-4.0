726689e6b9c130bdfbb6e894addb5ea4cd6cfb69
ELISONHG <61664393+ELISONHG@users.noreply.github.com>
2020-03-02 22:08:10 -0600

    Add files via upload

---
+++ perfil.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 13:22:23 2020
+
+@author: elison
+"""
+Name="BIG"
+Last_name="PAPI"
+Age="20 years"
+Height="1.75mts"
+ID="Id 193112020"
+Job="SUPERHERO"
+
+print('{}'.format(Last_name))
+print('{}'.format(Name))
+print('{}'.format(Age))
+print('{}'.format(Height))
+print('{}'.format(ID))
+print('{}'.format(Job))
